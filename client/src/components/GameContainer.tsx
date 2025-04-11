4536d0d358abeb39cd17ae64995b1fb5873a5efc
ben-lehman <ben.a.lehman@gmail.com>
2025-04-11 12:10:29 -0700

    added continent grouping data

--- client/src/components/GameContainer.tsx
+++ client/src/components/GameContainer.tsx
@@ -6,7 +6,7 @@ import {
 } from "react-leaflet";
 import { Feature, FeatureCollection } from "geojson";
 import { useCallback, useEffect, useState } from "react";
-import countryGeoJSON from "../../../data/countriesv4.geo.json";
+import countryGeoJSON from "../../../data/countriesv4filtered.geo.json";
 import "leaflet/dist/leaflet.css";
 import { StyleFunction, LatLngBoundsExpression } from "leaflet";
 import { Button } from "@/components/ui/button";
@@ -104,16 +104,26 @@ async function fetchNextCountry(currentCountry: string): Promise<CountryData> {
 }
 
 function GameContainer() {
+  const [gameStarted, setGameStarted] = useState(false);
   const [targetCountry, setTargetCountry] = useState<CountryData | null>(null);
   const [countryBounds, setCountryBounds] = useState<[number, number, number, number] | null>(null);
   const [message, setMessage] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
+  const [attempts, setAttempts] = useState(0);
+  const [isCorrect, setIsCorrect] = useState(false);
+
+  const startGame = () => {
+    setGameStarted(true);
+    selectRandomCountry();
+  }
 
   // Function to select a random country
   const selectRandomCountry = useCallback(async () => {
     setIsLoading(true);
     setError(null);
+    setAttempts(0);
+    setIsCorrect(false);
 
     try {
       const data = await fetchNextCountry(targetCountry?.adminISO || "ZZZ");
@@ -132,16 +142,18 @@ function GameContainer() {
 
   const onCountryClick = useCallback(
     (feature: Feature) => {
+      setAttempts(attempts + 1);
       const countryName = feature.properties?.name;
       const targetName = targetCountry?.name;
 
       if (targetName && countryName && countryName === targetName) {
+        setIsCorrect(true);
         setMessage(`Correct! You found ${countryName}`);
       } else if (countryName) {
         setMessage(`Wrong! That was ${countryName}. Try again!`);
       }
     },
-    [targetCountry],
+    [targetCountry, attempts],
   );
 
   const countryStyle: StyleFunction<any> = useCallback(
@@ -156,9 +168,19 @@ function GameContainer() {
         };
       }
 
-      if (feature.properties?.name === targetCountry?.name) {
+      if (isCorrect && feature.properties?.name === targetCountry?.name) {
         return {
           fillColor: "green",
+          weight: 1,
+          opacity: 1,
+          color: "white",
+          fillOpacity: 1,
+        };
+      }
+
+      if (feature.properties?.name === targetCountry?.name && attempts >= 3) {
+        return {
+          fillColor: "lightblue",
           weight: 1,
           opacity: 1,
           color: "white",
@@ -174,29 +196,16 @@ function GameContainer() {
         fillOpacity: 1,
       };
     },
-    [targetCountry],
+    [targetCountry, attempts],
   );
 
   return (
-    <div className="game-container">
-      <div className="controls">
-        <Button onClick={selectRandomCountry} disabled={isLoading}>
-          {isLoading ? "Loading..." : "Select Random Country"}
-        </Button>
-        {error && <div className="error-message">{error}</div>}
-        {targetCountry && (
-          <div className="target-country">
-            Target Country: {targetCountry.name}
-          </div>
-        )}
-        {message && (
-          <div
-            className={`message ${message.startsWith("Correct") ? "correct" : "wrong"}`}
-          >
-            {message}
-          </div>
-        )}
-      </div>
+    <div className="game-container w-full h-full p-4">
+      {!gameStarted && (
+        <div className="absolute w-full h-full flex justify-center items-center z-9999">
+          <Button onClick={() => startGame()}>Start Game</Button>
+        </div>
+      )}
       <MapContainer
         center={[20, 0]}
         zoom={2}
@@ -220,6 +229,24 @@ function GameContainer() {
         />
         <ZoomToCountry bounds={countryBounds} expandedBounds={countryBounds} />
       </MapContainer>
+      <div className="controls flex flex-col gap-2 justify-center items-center">
+        {error && <div className="error-message">{error}</div>}
+        {targetCountry && (
+          <div className="target-country">
+            Target Country: {targetCountry.name}
+          </div>
+        )}
+        {message && (
+          <div
+            className={`message ${message.startsWith("Correct") ? "correct" : "wrong"}`}
+          >
+            {message}
+            {message.startsWith("Correct") && attempts > 0 && (
+              <Button onClick={() => selectRandomCountry()}>Next Country</Button>
+            )}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
--- server/tools/parsedata/main.go
+++ server/tools/parsedata/main.go
@@ -6,6 +6,8 @@ import (
 	"io"
 	"log"
 	"os"
+	"slices"
+	"strings"
 
 	"github.com/ben-lehman/countryclicker/server/internal/countries"
 )
@@ -39,10 +41,67 @@ type Properties struct {
 }
 
 func main() {
+  filterGeoJSON()
+	geoJSONToCountryList()
+	groupCountryListByContinent()
+
+	// geoJSONForClient()
+
+}
+
+func filterGeoJSON() {
+	inputPath := "../../../data/countriesv4.geo.json"
+	outputPath := "../../../data/countriesv4filtered.geo.json"
+
+	jsonFile, err := os.Open(inputPath)
+	if err != nil {
+		log.Fatalf("Unable to open country data file at %s: %v", inputPath, err)
+	}
+	defer jsonFile.Close()
+
+	byteData, err := io.ReadAll(jsonFile)
+	if err != nil {
+		log.Fatalf("Unable to read jsonFile %s: %v", inputPath, err)
+	}
+
+	var featureCollection FeatureCollection
+	err = json.Unmarshal(byteData, &featureCollection)
+	if err != nil {
+		log.Fatalf("Unable to unmarshal byteData: %v", err)
+	}
+
+	filterCountryISO := []string{"ATA", "ATF"}
+
+	var filteredCollection FeatureCollection
+	filteredCollection.Type = featureCollection.Type
+	for _, feature := range featureCollection.Features {
+		if !slices.Contains(filterCountryISO, feature.Properties.AdminISO) {
+			filteredCollection.Features = append(filteredCollection.Features, feature)
+		}
+	}
+
+	parsedJSON, err := json.Marshal(filteredCollection)
+	if err != nil {
+		log.Fatalf("Unable to marshal filtered collection: %v", err)
+	}
+
+	parsedFile, err := os.Create(outputPath)
+	if err != nil {
+		log.Fatalf("Unable to create file at %s, %v", outputPath, err)
+	}
+	defer parsedFile.Close()
+
+	err = os.WriteFile(outputPath, parsedJSON, 0644)
+	if err != nil {
+		log.Fatalf("Unable to copy file %s to %s, %v", inputPath, outputPath, err)
+	}
+
+	fmt.Printf("Successfully filtered data from %s to %s", inputPath, outputPath)
+}
+
+func geoJSONToCountryList() {
 	inputPath := "../../../data/countriesv4.geo.json"
 	outputPath := "../../data/countries-list.json"
-
-	geoJSONForClient()
 
 	jsonFile, err := os.Open(inputPath)
 	if err != nil {
@@ -76,6 +135,9 @@ func main() {
 	}
 
 	parsedJSON, err := json.Marshal(parsedCountries)
+	if err != nil {
+		log.Fatalf("Unable to marshal parsed countries: %v", err)
+	}
 
 	parsedFile, err := os.Create(outputPath)
 	if err != nil {
@@ -89,6 +151,58 @@ func main() {
 	}
 
 	fmt.Printf("Successfull parsed data from %s to %s", inputPath, outputPath)
+}
+
+func groupCountryListByContinent() {
+	inputPath := "../../data/countries-list.json"
+	outputDir := "../../data/"
+
+	// read countries list
+	jsonFile, err := os.Open(inputPath)
+	if err != nil {
+		log.Fatalf("Unable to open country data file at %s: %v", inputPath, err)
+	}
+	defer jsonFile.Close()
+
+	byteData, err := io.ReadAll(jsonFile)
+	if err != nil {
+		log.Fatalf("Unable to read jsonFile %s: %v", inputPath, err)
+	}
+
+	var countriesList countries.CountriesData
+	err = json.Unmarshal(byteData, &countriesList)
+	if err != nil {
+		log.Fatalf("Unable to unmarshal byteData: %v", err)
+	}
+
+	// create map of countries with continent keys
+	Continents := make(map[string]countries.CountriesData)
+	for _, country := range countriesList {
+		cont := country.Continent
+		Continents[cont] = append(Continents[cont], country)
+	}
+
+	// each continent key writes to a <continent>-countries-list.json
+	for cont, countries := range Continents {
+		jsonData, err := json.Marshal(countries)
+		if err != nil {
+			log.Fatalf("Unable to marshal countries: %v", err)
+		}
+
+		outputPath := fmt.Sprintf("%s%s-countries-list.json", outputDir, strings.ToLower(cont))
+		parsedFile, err := os.Create(outputPath)
+		if err != nil {
+			log.Fatalf("Unable to create file at %s, %v", outputPath, err)
+		}
+		defer parsedFile.Close()
+
+		err = os.WriteFile(outputPath, jsonData, 0644)
+		if err != nil {
+			log.Fatalf("Unable to copy file %s to %s, %v", inputPath, outputPath, err)
+		}
+
+		fmt.Printf("Successfull parsed data from %s to %s", inputPath, outputPath)
+	}
 }
 
 func geoJSONForClient() {
