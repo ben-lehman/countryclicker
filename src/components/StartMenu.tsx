97038df4b797488e5db99e1e39e7ec148815f474
ben-lehman <ben.a.lehman@gmail.com>
2025-06-18 12:08:08 -0400

    updated start menu positioning on mobile

--- src/components/StartMenu.tsx
+++ src/components/StartMenu.tsx
@@ -21,7 +21,7 @@ function StartMenu({ handleGameStart }: StartMenuProps) {
   );
 
   return (
-    <div className="absolute top-[50%] left-[50%] w-[350px] h-[280px] -ml-[175px] -mt-[140px] flex flex-wrap gap-x-4 justify-center items-center p-8 z-9999 bg-rp-base border-2 border-color-rp-gold shadow-md shadow-black">
+    <div className="start-menu flex flex-wrap gap-x-4 justify-center items-center p-8 z-9999 bg-rp-base border-2 border-color-rp-gold shadow-md shadow-black">
       <span className="text-rp-text">Select a Game Mode</span>
       <Tabs
         defaultValue="world"
